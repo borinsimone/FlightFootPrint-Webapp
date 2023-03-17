1944b91cbc0e2a49428134317fe5b45085d98584
borinsimone <Borin.simone1997@gmail.com>
2023-03-17 15:13:15 +0100

    ui improvement

--- src/FootprintCalculator.js
+++ src/FootprintCalculator.js
@@ -229,7 +229,9 @@ function FootprintCalculator() {
       {/* OUTPUT */}
       <div className="output relative flex flex-col gap-4 md:gap-6 h-[50%] w-[90%] lg:w-[70%] capitalize lg:flex-row pt-10 ">
         <div
-          className={`general-data  ml-auto flex flex-col gap-4 lg:gap-6 w-full lg:w-[40%]  `}
+          className={`general-data   flex flex-col gap-4 lg:gap-6 w-full lg:w-[50%] duration-500 ${
+            emissionData ? "opacity-1" : "opacity-0"
+          } `}
         >
           <div className="total flex items-center gap-2 h-8 md:h-12 bg-[#F8F4EA]   rounded-lg p-2 md:p-3 ">
             <span>total C02:</span>
@@ -256,7 +258,9 @@ function FootprintCalculator() {
         flgihtExist ? (
           <div
             className={`recap w-full lg:w-[30%] bg-[#F8F4EA] rounded-lg flex flex-col justify-center items-center capitalize relative 
-             lg:h-[200px]    `}
+             lg:h-[200px] duration-500 ${
+               emissionData ? "opacity-1" : "opacity-0"
+             } `}
           >
             <div className="recap-logo absolute left-3 top-3">
               recap:
@@ -280,7 +284,9 @@ function FootprintCalculator() {
         )}
 
         <div
-          className={`multi-flight overflow-y-scroll flex flex-col gap-4 md:gap-6 ${
+          className={`multi-flight ${
+            emissionData ? "" : ""
+          } overflow-y-scroll flex flex-col gap-4  md:gap-6 ${
             emissionData && emissionData.legs.length > 1
               ? "lg:w-[50%]"
               : "lg:w-0"
@@ -348,122 +354,6 @@ function FootprintCalculator() {
               ))
             : ""}
         </div>
-
-        {/* {emissionData &&
-        emissionData.legs.length === 1 &&
-        flgihtExist ? (
-          <div
-            className={`recap w-full bg-[#F8F4EA] rounded-lg flex flex-col justify-center items-center capitalize relative 
-             lg:h-[200px]    `}
-          >
-            <div className="recap-logo absolute left-3 top-3">
-              recap:
-            </div>
-            <div className="recap-container my-3 w-[50%] lg:w-[40%]">
-              <div className="recap-from-to  w-full flex justify-between">
-                <span>flight:</span>
-                {`${legs[0].from} - ${legs[0].to}`}
-              </div>
-              <div className="recap-class w-full flex justify-between">
-                <span>class:</span> {`${legs[0].class}`}
-              </div>
-              <div className="recap-passengers w-full flex justify-between">
-                <span>n. passengers:</span>
-                {`${legs[0].passengers}`}
-              </div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-        <div
-          className={`general-data flex flex-col gap-4 lg:gap-6 lg:w-[50%] ${
-            legList.length > 1
-              ? ""
-              : "lg:h-[200px] lg:justify-evenly"
-          }  `}
-        >
-          <div className="total flex items-center gap-2 h-8 md:h-12 bg-[#F8F4EA]   rounded-lg p-2 md:p-3 ">
-            <span>total C02:</span>
-            {emissionData
-              ? emissionData.co2e.toFixed(2)
-              : ""}
-            {emissionData ? emissionData.co2e_unit : ""}
-          </div>
-          <div className="individual  flex items-center gap-2 h-8 md:h-12 bg-[#F8F4EA]   rounded-lg p-2 md:p-3 ">
-            <span>C02/passengers: </span>
-            {emissionData
-              ? (
-                  emissionData.co2e / totalPassengers
-                ).toFixed(2)
-              : ""}
-            {emissionData ? emissionData.co2e_unit : ""}
-          </div>
-        </div>
-
-        <div className="multi-flight overflow-y-scroll flex flex-col gap-4 md:gap-6  lg:w-[50%] ">
-          {emissionData && emissionData.legs.length > 1
-            ? emissionData.legs.map((item, i) => (
-                <div
-                  key={i}
-                  className="individual-flight flex flex-col bg-[#F8F4EA] cursor-pointer   justify-center rounded-lg  "
-                  onClick={() => {
-                    setIsDropdownOpen((prevState) =>
-                      prevState.map((item, index) =>
-                        index === i ? !item : false
-                      )
-                    );
-                  }}
-                >
-                  <div className="emission-header md:h-12 p-2 md:p-3 w-full flex items-center justify-between">
-                    <span className="">
-                      recap. flight {i + 1}
-                    </span>
-                    {`${legs[i].from} - ${legs[i].to}`}
-                  </div>
-
-                  <div
-                    className={`dropdown emission flex  overflow-hidden px-2 md:px-3  w-full 
-            ${
-              isDropdownOpen[i]
-                ? " opacity-1 h-[4em] dropdown-open "
-                : "opacity-0 h-0 dropdown-close"
-            }`}
-                  >
-                    <div className="left w-[50%] flex flex-col gap-2 ">
-                      <div className="total flex items-center gap-1">
-                        <span className="">total:</span>
-                        {emissionData.legs[i].co2e.toFixed(
-                          2
-                        )}
-                        {emissionData.legs[i].co2e_unit}
-                      </div>
-                      <div className="individual flex gap-1">
-                        <span>individual:</span>
-                        {(
-                          emissionData.legs[i].co2e /
-                          legs[i].passengers
-                        ).toFixed(2)}
-                        {emissionData
-                          ? emissionData.co2e_unit
-                          : ""}
-                      </div>
-                    </div>
-                    <div className="right w-[50%] flex flex-col gap-2 items-end ">
-                      <div className="drop-class flex gap-1">
-                        <span>class:</span>
-                        {`${legs[i].class}`}
-                      </div>
-                      <div className="drop-passengers flex gap-1">
-                        <span>n. passengers:</span>
-                        {`${legs[i].passengers}`}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            : ""}
-        </div> */}
       </div>
     </div>
   );
