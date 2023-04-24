062879afb5e8adde96f8a612746dace17801208b
borinsimone <Borin.simone1997@gmail.com>
2023-04-24 14:31:35 +0200

    github react router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ContextProvider } from "./context/context";
-import { BrowserRouter } from "react-router-dom";
+import {
+  HashRouter,
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </BrowserRouter>
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </React.StrictMode>
 );
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { ContextProvider } from "./context/context";
+// import { BrowserRouter } from "react-router-dom";
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root")
+// );
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <ContextProvider>
+//         <App />
+//       </ContextProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
